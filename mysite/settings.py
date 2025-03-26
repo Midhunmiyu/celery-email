4c856645b88886d9b2e71d2dffddbb6c60d4c217
Midhun Raj E K <120627714+Midhunmiyu@users.noreply.github.com>
2025-03-26 08:43:34 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fastmail@smtp-mail.noviindus.co.in'
-EMAIL_HOST_PASSWORD = 'lkccuuumldssxost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
